595eba1cf42e5b8b20cce547d0da6c1398472c7c
Mukesh <mukeshjha2007@gmail.com>
2017-07-13 18:52:56 +0530

    gridview added

--- includes/views/menuView.js
+++ includes/views/menuView.js
@@ -7,6 +7,32 @@ import {
   View
 } from 'react-native';
 import ImageSlider from 'react-native-image-slider';
+import GridView from 'react-native-gridview';
+const itemsPerRow = 3;
+
+
+// Use data from an array...
+const data = Array(20)
+  .fill(null)
+  .map((item, index) => index + 1);
+
+  const randomData = [];
+  for (let i = 0; i < data.length; i) {
+    const endIndex = Math.max(Math.round(Math.random() * itemsPerRow), 1) + i;
+    randomData.push(data.slice(i, endIndex));
+    i = endIndex;
+  }
+  const dataSource = new GridView.DataSource({
+    rowHasChanged: (r1, r2) => r1 !== r2,
+  }).cloneWithRows(randomData);
+
+
+
+
+
+
+
+
 
 export default class MenuView extends Component {
   constructor(props, context) {
@@ -38,13 +64,23 @@ export default class MenuView extends Component {
                    images={[
                         require('../image/logo.jpg'),
                         require('../image/logo.jpg'),
-                       require('../image/logo.jpg'),
+                        require('../image/logo.jpg'),
                    ]}
                    position={this.state.position}
                    onPositionChanged={position => this.setState({position})}/>
-
-                   <View style ={ {flex: .7, marginTop:10,backgroundColor:'red'}} >
-
+                   <View style ={ {flex: .7, marginTop:10}} >
+                   <GridView
+                         data={data}
+                         dataSource={null}
+                         itemsPerRow={itemsPerRow}
+                         renderItem={(item, sectionID, rowID, itemIndex, itemID) => {
+                           return (
+                             <View style={{ flex: 1, backgroundColor: '#8F8', borderWidth: 1 }}>
+                               <Text>{`${item} (${sectionID}-${rowID}-${itemIndex}-${itemID})`}</Text>
+                             </View>
+                           );
+                         }}
+                       />
 
 
 
